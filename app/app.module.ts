c9b202e0f628adb10a87e6370a153e7d98d7e3f2
nahwinrajan <nahwin.rajan@gmail.com>
2016-10-30 23:01:49 +0700

    done add service-github

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent }  from './app.component';
-import { ProfileComponent } from './components/profile/profile.component'
+import { AppComponent }     from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   declarations: [ AppComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ app/services/github.service.ts
@@ -0,0 +1,26 @@
+// for dependencies and dependencies injections;
+// as we need to inject this service to any component needed
+import {Injectable} from '@angular/core';
+// because we need to make http request (specifically get for this)
+import {Http, Headers} from '@angular/http';
+// observable, this is react extension; return response with observable and subscribe
+// to that observable component and display information
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+  private username:string;
+  private client_id = '7bfff7c9634783b93bf3';
+  private client_secret = '6b1bcbd4b7fd6128585921b38c47122c6a9525c4';
+
+  constructor(private _http:Http){
+    console.log('Github Service ready!!');
+    this.username = 'nahwinrajan';
+  }
+
+  getUser() {
+    return this._http.get('http://api.github.com/users/' + this.username +
+      '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+      .map(res => res.json());
+  }
+}
