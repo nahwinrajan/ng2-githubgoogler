99dbc6642148202cb6719837170c71d95bacd094
nahwinrajan <nahwin.rajan@gmail.com>
2016-10-31 00:11:20 +0700

    finish add search user feature

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -15,7 +15,6 @@ export class GithubService{
 
   constructor(private _http:Http){
     console.log('Github Service ready!!');
-    this.username = 'nahwinrajan';
   }
 
   getUser() {
@@ -27,5 +26,8 @@ export class GithubService{
     return this._http.get('http://api.github.com/users/' + this.username +
       '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
       .map(res => res.json());
+  }
+  updateUser(username:string){
+    this.username = username;
   }
 }
